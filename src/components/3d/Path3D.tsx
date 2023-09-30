19a20e5689658fb16220fc193c4051d2805ca8ed
KostyalBalint <kostyal.balintnak@gmail.com>
2023-09-30 02:09:55 +0200

    path3d

--- src/components/3d/Path3D.tsx
+++ src/components/3d/Path3D.tsx
@@ -1,4 +1,5 @@
-import { CatmullRomCurve3, Color, ShaderMaterial, TubeGeometry, Vector2, Vector3 } from 'three'
+import { CatmullRomCurve3, TubeGeometry, Vector2, Vector3 } from 'three'
+import { material } from './helpers/gradientMaterial'
 
 interface Path3DParams {
     path: Vector2[]
@@ -13,53 +14,6 @@ export const Path3D = (props: Path3DParams) => {
         false
     )
 
-    const material = new ShaderMaterial({
-        uniforms: {
-            color1: {
-                value: new Color('#a9c5fd'),
-            },
-            opacity1: {
-                value: 1,
-            },
-            color2: {
-                value: new Color('#a9c5fd'),
-            },
-            opacity2: {
-                value: 0.3,
-            },
-        },
-        vertexShader: `
-            varying vec2 vUv;
-        
-            void main() {
-              vUv = uv;
-              gl_Position = projectionMatrix * modelViewMatrix * vec4(position,1.0);
-            }
-          `,
-        fragmentShader: `
-            uniform vec3 color1;
-            uniform vec3 color2;
-            uniform float opacity1;
-            uniform float opacity2;
-          
-            varying vec2 vUv;
-            
-            void main() {
-                //gl_FragColor = vec4(mix(color1, color2, vUv.x), mix(opacity1, opacity2, vUv.x));
-                
-                // y < 0 = transparent, > 1 = opaque
-                float alpha = smoothstep(opacity1, opacity2, vUv.x);
-            
-                // y < 1 = color1, > 2 = color2
-                float colorMix = smoothstep(0.0, 1.0, vUv.x);
-            
-                gl_FragColor = vec4(mix(color1, color2, colorMix), alpha);
-            }
-          `,
-        transparent: true,
-    })
-
-    //const geometry = new BufferGeometry().setFromPoints(points)
     const tubeGeometry = new TubeGeometry(curve, 100, props.radius, 10, props.closed)
     tubeGeometry.scale(1, 0.05, 1)
     return (
---
+++ src/components/3d/helpers/gradientMaterial.ts
@@ -0,0 +1,47 @@
+import { Color, ShaderMaterial } from 'three'
+
+export const material = new ShaderMaterial({
+    uniforms: {
+        color1: {
+            value: new Color('#a9c5fd'),
+        },
+        opacity1: {
+            value: 1,
+        },
+        color2: {
+            value: new Color('#a9c5fd'),
+        },
+        opacity2: {
+            value: 0.3,
+        },
+    },
+    vertexShader: `
+            varying vec2 vUv;
+        
+            void main() {
+              vUv = uv;
+              gl_Position = projectionMatrix * modelViewMatrix * vec4(position,1.0);
+            }
+          `,
+    fragmentShader: `
+            uniform vec3 color1;
+            uniform vec3 color2;
+            uniform float opacity1;
+            uniform float opacity2;
+          
+            varying vec2 vUv;
+            
+            void main() {
+                //gl_FragColor = vec4(mix(color1, color2, vUv.x), mix(opacity1, opacity2, vUv.x));
+                
+                // y < 0 = transparent, > 1 = opaque
+                float alpha = smoothstep(opacity1, opacity2, vUv.x);
+            
+                // y < 1 = color1, > 2 = color2
+                float colorMix = smoothstep(0.0, 1.0, vUv.x);
+            
+                gl_FragColor = vec4(mix(color1, color2, colorMix), alpha);
+            }
+          `,
+    transparent: true,
+})
